dcb3c1bf6975ee45ea999b89c950ced8c2821b41
dens7 <tambi@wisc.edu>
2020-11-02 20:54:03 -0600

    Completed unPinPage and Destructor

--- src/buffer.cpp
+++ src/buffer.cpp
@@ -13,7 +13,7 @@
 #include "exceptions/page_pinned_exception.h"
 #include "exceptions/bad_buffer_exception.h"
 #include "exceptions/hash_not_found_exception.h"
- #include <stdint.h>
+
 
 namespace badgerdb { 
 
@@ -21,25 +21,29 @@ namespace badgerdb {
 // Constructor of the class BufMgr
 //----------------------------------------
 
-BufMgr::BufMgr(std::uint32_t bufs)
-	: numBufs(bufs) {
+BufMgr::BufMgr(std::uint32_t bufs): numBufs(bufs) {
+	
 	bufDescTable = new BufDesc[bufs];
+	for (FrameId i = 0; i < bufs; i++) 
+  	{
+  		bufDescTable[i].frameNo = i;
+  		bufDescTable[i].valid = false;
+  	}
 
-  for (FrameId i = 0; i < bufs; i++) 
-  {
-  	bufDescTable[i].frameNo = i;
-  	bufDescTable[i].valid = false;
-  }
-
-  bufPool = new Page[bufs];
-
-  int htsize = ((((int) (bufs * 1.2))*2)/2)+1;
-  hashTable = new BufHashTbl (htsize);  // allocate the buffer hash table
-  clockHand = bufs - 1;
+  	bufPool = new Page[bufs];
+	int htsize = ((((int) (bufs * 1.2))*2)/2)+1;
+  	hashTable = new BufHashTbl (htsize);  // allocate the buffer hash table
+  	clockHand = bufs - 1;
 }
 
 
-BufMgr::~BufMgr() {
+BufMgr::~BufMgr() 
+{
+	for(std::uint32_t i = 0; i < numBufs; i++)	//Flushes out all dirty pages
+		if(bufDescTable[i].dirty) flushFile(bufDescTable[i].file);
+		
+	delete [] bufPool;
+	delete [] bufDescTable;	
 }
 
 void BufMgr::advanceClock()
@@ -50,17 +54,18 @@ void BufMgr::advanceClock()
 
 void BufMgr::allocBuf(FrameId & frame) 
 {
-	found = false;
-	std::uint32_t i = 0
+	bool found = false;
+	std::uint32_t i;
 	for(i = 0; i < numBufs; i++) 
 	{
 		advanceClock();
 		if (!bufDescTable[clockHand].valid) 
+		{
 			found = true;
 			break;
-		else if (bufDescTable[clockHand].refbit) 
-			bufDescTable[clockHand].refbit = false;
-		else if (bufDetuscTable[clockHand].pinCnt != 0);
+		}
+		else if (bufDescTable[clockHand].refbit) bufDescTable[clockHand].refbit = false;
+		else if (bufDescTable[clockHand].pinCnt != 0);
 		else 
 		{
 			found = true;
@@ -103,6 +108,18 @@ void BufMgr::readPage(File* file, const PageId pageNo, Page*& page)
 
 void BufMgr::unPinPage(File* file, const PageId pageNo, const bool dirty) 
 {
+	FrameId frameNum;
+	try {
+		hashTable->lookup(file, pageNo, frameNum);
+		if (bufDescTable[frameNum].pinCnt == 0) 
+			throw PageNotPinnedException(file->filename(), pageNo, frameNum);
+		bufDescTable[frameNum].pinCnt--;
+		if(dirty) bufDescTable[frameNum].dirty = true;
+	}
+	catch(HashNotFoundException e)
+	{
+
+	}
 }
 
 void BufMgr::allocPage(File* file, PageId &pageNo, Page*& page) 
@@ -129,15 +146,14 @@ void BufMgr::printSelf(void)
   	BufDesc* tmpbuf;
 	int validFrames = 0;
   
-  for (std::uint32_t i = 0; i < numBufs; i++)
-	{
-  	tmpbuf = &(bufDescTable[i]);
+ 	for (std::uint32_t i = 0; i < numBufs; i++) {
+  		tmpbuf = &(bufDescTable[i]);
 		std::cout << "FrameNo:" << i << " ";
 		tmpbuf->Print();
 
   	if (tmpbuf->valid == true)
     	validFrames++;
-  }
+}
 
 	std::cout << "Total Number of Valid Frames:" << validFrames << "\n";
 }
